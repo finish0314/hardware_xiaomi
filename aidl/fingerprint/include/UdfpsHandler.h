f73a49e0cde5230c2f3b8b6a6062ab084c3f90c7
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-28 16:26:08 +0200

    aidl: fingerprint: Migrate Xiaomi implementation to AIDL
    Co-authored-by: IPSBHANGU <inderpreetbhangu2@gmail.com>
    Change-Id: Ib410ef5d92747deb9af34b3eb88f620c75797618
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- aidl/fingerprint/include/UdfpsHandler.h
+++ aidl/fingerprint/include/UdfpsHandler.h
@@ -19,6 +19,10 @@ class UdfpsHandler {
 
     virtual void onAcquired(int32_t result, int32_t vendorCode) = 0;
     virtual void cancel() = 0;
+
+    virtual void preEnroll() = 0;
+    virtual void enroll() = 0;
+    virtual void postEnroll() = 0;
 };
 
 struct UdfpsHandlerFactory {
